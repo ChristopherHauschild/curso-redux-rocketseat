f281a7189ebccd5a460d96051d8d51cea3db26c2
ChristopherHauschild <chrishaus01@gmail.com>
2020-10-14 12:57:14 -0300

    modules 1 and 2 finished

---
+++ redux-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import Catalog from "./components/Catalog";
+import Cart from "./components/Cart";
+
+import store from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Catalog />
+      <Cart />
+    </Provider>
+  );
+};
+
+export default App;
